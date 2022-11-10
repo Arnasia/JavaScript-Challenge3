5eb827a28564001bc115d5f1cc713d2b16e9b232
Arnasia Jackson <arnasial.jackson@gmail.com>
2022-11-09 20:07:00 -0500

    add a series of prompts and listed criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,23 @@
 // Assignment code here
 
+function generatePassword() {
+var pwdLength = prompt("How many characters would you like your password to be?")
+
+  if (pwdLength < 8 || pwdLength > 128){
+      alert("Length must be 8-128 characters")
+  }
+
+  if (pwdLength >= 8 && pwdLength <= 128){
+  var uppercase = confirm ("Would you like to use uppercase letters?")
+  var lowercase = confirm("Would you like to use lowercase letters?")
+  var numbers = confirm("would you like to use numbers?")
+  var symbols = confirm("would you like to use special characters?")
+  }    
+
+  if (uppercase != true && lowercase != true && numbers != true && symbols != true){
+      alert("You must select at least one character type!")
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
