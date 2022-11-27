e0131377fa9901f321c3a8f124d645cfcee7d803
Arnasia Jackson <arnasial.jackson@gmail.com>
2022-11-26 21:03:53 -0600

    set variables and created a series of prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,35 @@
 // Assignment code here
-
-function generatePassword() {
-var pwdLength = prompt("How many characters would you like your password to be?")
-
-  if (pwdLength < 8 || pwdLength > 128){
-      alert("Length must be 8-128 characters")
-  }
-
-  if (pwdLength >= 8 && pwdLength <= 128){
-  var uppercase = confirm ("Would you like to use uppercase letters?")
-  var lowercase = confirm("Would you like to use lowercase letters?")
-  var numbers = confirm("would you like to use numbers?")
-  var symbols = confirm("would you like to use special characters?")
-  }    
-
-  if (uppercase != true && lowercase != true && numbers != true && symbols != true){
-      alert("You must select at least one character type!")
-  }
+let numbers = [0,1,2,3,4,5,6,7,8,9]
+let upperCase = ["A","B","C","D","E","F"]
+let lowerCase = ["a","b","c","d","e","f"]
+let specialChars = ["!","#","?","*","%","&"]
+let userChoice = []
+function generatePassword () {
+  
+let passwordLength = prompt ("Enter Length of password between 8-128 Characters")
+let numberBoolean = confirm ("Should password consist of numbers?")
+let upperCaseBoolean = confirm ("Should password consist of uppercase characters?")
+let lowerCaseBoolean = confirm ("Should password consist of lowercase characters?")
+let specialCharsBoolean = confirm ("Should password consist of special characters?")
+console.log(numberBoolean)
+if (numberBoolean) {
+userChoice = userChoice.concat(numbers)
 }
+
+
+if (upperCaseBoolean) {
+  userChoice = userChoice.concat(upperCase)
+}
+if (lowerCaseBoolean) {
+  userChoice = userChoice.concat(lowerCase)
+}
+if (specialCharsBoolean) {
+  userChoice = userChoice.concat(specialChars)
+}
+console.log(userChoice)
+}
+//make password appear in display box
+// document.getElementById("display").value = password;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -28,7 +40,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
